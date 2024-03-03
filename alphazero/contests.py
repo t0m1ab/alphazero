eeee510cc1337fb65c8e505b8cb62a5fb357d401
Tom Labiausse <tom01lab@gmail.com>
2024-03-03 18:29:31 +0100

    update contests.py and rename game_ui.py -> game.cli.py

--- alphazero/contests.py
+++ alphazero/contests.py
@@ -4,6 +4,7 @@ from alphazero.players import HumanPlayer, RandomPlayer, GreedyPlayer, MCTSPlaye
 from alphazero.arena import Arena
 from alphazero.base import Player
 from alphazero.games.othello import OthelloBoard, OthelloNet
+from alphazero.games.tictactoe import TicTacToeBoard, TicTacToeNet
 
 
 def show_results(player1: Player, player2: Player, stats: dict, ):
@@ -15,7 +16,7 @@ def show_results(player1: Player, player2: Player, stats: dict, ):
     print(f"- {player2} wins = {len(stats['player2'])}/{n_rounds}")
 
 
-def MCTS_vs_Random(n_rounds: int = 2, n_process: int = 1, verbose: bool = False):
+def MCTS_vs_Random_Othello(n_rounds: int = 2, n_process: int = 1, verbose: bool = False):
     """ Organize a contest between MCTSPlayer and RandomPlayer. """
 
     player1 = MCTSPlayer(compute_time=0.2)
@@ -31,11 +32,10 @@ def MCTS_vs_Random(n_rounds: int = 2, n_process: int = 1, verbose: bool = False)
 
     show_results(player1, player2, stats)
 
-
-def MCTS_vs_Greedy(n_rounds: int = 2, n_process: int = 1, verbose: bool = False):
+def MCTS_vs_Greedy_Othello(n_rounds: int = 2, n_process: int = 1, verbose: bool = False):
     """ Organize a contest between MCTSPlayer and RandomPlayer. """
 
-    player1 = MCTSPlayer(compute_time=0.2)
+    player1 = MCTSPlayer(n_sim=1000)
     player2 = GreedyPlayer()
     board = OthelloBoard(6)
 
@@ -48,8 +48,7 @@ def MCTS_vs_Greedy(n_rounds: int = 2, n_process: int = 1, verbose: bool = False)
 
     show_results(player1, player2, stats)
 
-
-def AZ_vs_Greedy(n_rounds: int = 2, n_process: int = 1, verbose: bool = False):
+def AZ_vs_Greedy_Othello(n_rounds: int = 2, n_process: int = 1, verbose: bool = False):
     """ Organize a contest between AlphaZeroPlayer and RandomPlayer. """
 
     n = 6
@@ -70,8 +69,66 @@ def AZ_vs_Greedy(n_rounds: int = 2, n_process: int = 1, verbose: bool = False):
     show_results(player1, player2, stats)
 
 
+def MCTS_vs_Random_TicTacToe(n_rounds: int = 2, n_process: int = 1, verbose: bool = False):
+    """ Organize a contest between MCTSPlayer and RandomPlayer. """
+
+    player1 = MCTSPlayer(compute_time=0.2)
+    player2 = RandomPlayer()
+    board = TicTacToeBoard()
+
+    arena = Arena(player1, player2, board)
+
+    if n_process == 1: # sequential
+        stats = arena.play_games(n_rounds, return_stats=True, verbose=verbose)
+    else: # parallel (use all available cores if n_process is None)
+        stats = arena.play_games_in_parallel(n_rounds, n_process=n_process, verbose=verbose, return_stats=True)
+
+    show_results(player1, player2, stats)
+
+def MCTS_vs_Greedy_TicTacToe(n_rounds: int = 2, n_process: int = 1, verbose: bool = False):
+    """ Organize a contest between MCTSPlayer and RandomPlayer. """
+
+    player1 = MCTSPlayer(n_sim=1000)
+    player2 = GreedyPlayer()
+    board = TicTacToeBoard()
+
+    arena = Arena(player1, player2, board)
+
+    if n_process == 1: # sequential
+        stats = arena.play_games(n_rounds, return_stats=True, verbose=verbose)
+    else: # parallel (use all available cores if n_process is None)
+        stats = arena.play_games_in_parallel(n_rounds, n_process=n_process, verbose=verbose, return_stats=True)
+
+    show_results(player1, player2, stats)
+
+def AZ_vs_Greedy_TicTacToe(n_rounds: int = 2, n_process: int = 1, verbose: bool = False):
+    """ Organize a contest between AlphaZeroPlayer and RandomPlayer. """
+
+    n = 6
+
+    nn = OthelloNet(n=n, device="cpu") # cuda or mps device will not work for parallel games
+    player1 = AlphaZeroPlayer(compute_time=0.2, nn=nn, verbose=verbose)
+    player2 = GreedyPlayer()
+    board = TicTacToeBoard()
+
+    arena = Arena(player1, player2, board)
+
+    # set the following verbose to True to show stats for each move
+    if n_process == 1: # sequential
+        stats = arena.play_games(n_rounds, return_stats=True, verbose=False)
+    else: # parallel (use all available cores if n_process is None)
+        stats = arena.play_games_in_parallel(n_rounds, n_process=n_process, verbose=False, return_stats=True)
+
+    show_results(player1, player2, stats)
+
+
 if __name__ == "__main__":
-    # MCTS_vs_Random(n_rounds=50, n_process=10, verbose=False)
-    # MCTS_vs_Greedy(n_rounds=50, n_process=10, verbose=False)
-    AZ_vs_Greedy(n_rounds=10, n_process=1, verbose=False)
+
+    # MCTS_vs_Random_Othello(n_rounds=50, n_process=10, verbose=False)
+    # MCTS_vs_Greedy_Othello(n_rounds=50, n_process=10, verbose=False)
+    AZ_vs_Greedy_Othello(n_rounds=10, n_process=1, verbose=False)
+
+    # MCTS_vs_Random_TicTacToe(n_rounds=50, n_process=10, verbose=False)
+    # MCTS_vs_Greedy_TicTacToe(n_rounds=50, n_process=10, verbose=False)
+    # AZ_vs_Greedy_TicTacToe(n_rounds=10, n_process=1, verbose=False)
 
--- alphazero/game_cli.py
+++ alphazero/game_cli.py
@@ -2,17 +2,30 @@ from alphazero.base import Board, Player
 from alphazero.players import HumanPlayer, RandomPlayer, GreedyPlayer, MCTSPlayer
 from alphazero.arena import Arena
 from alphazero.games.othello import OthelloBoard
+from alphazero.games.tictactoe import TicTacToeBoard
 
 
-def main():
-
+def othello():
+    """ Play Othello with CLI. """
     player1 = HumanPlayer()
     player2 = MCTSPlayer(compute_time=2.0, verbose=True)
     board = OthelloBoard(8)
-
     arena = Arena(player1, player2, board)
-
     arena.play_game(player2_starts=False, verbose=True, display=True)
+
+
+def tictactoe():
+    """ Play TicTacToe with CLI. """
+    player1 = HumanPlayer()
+    player2 = MCTSPlayer(n_sim=1000, verbose=True)
+    board = TicTacToeBoard()
+    arena = Arena(player1, player2, board)
+    arena.play_game(player2_starts=False, verbose=True, display=True)
+
+
+def main():
+    othello()
+    # tictactoe()
 
 
 if __name__ == "__main__":
